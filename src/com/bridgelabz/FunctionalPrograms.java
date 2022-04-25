2365f97db10cf372061304d88129783daefbdd26
vanibm15 <vanibm15@gmail.com>
2022-04-25 22:49:50 +0530

    added:perfect number program

--- src/com/bridgelabz/FunctionalPrograms.java
+++ src/com/bridgelabz/FunctionalPrograms.java
@@ -1,28 +1,32 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class FunctionalPrograms {
+    public static void main(String[] args) {
+        long n, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        n = sc.nextLong();
+        int i = 1;
+//executes until the condition becomes false
+        while (i <= n / 2) {
+            if (n % i == 0) {
+//calculates the sum of factors
+                sum = sum + i;
+            } //end of if
+//after each iteration, increments the value of variable i by 1
+            i++;
+        } //end of while
+//compares sum with the number
+        if (sum == n) {
+//prints if sum and n are equal
+            System.out.println(n + " is a perfect number.");
+        } //end of if
+        else
+//prints if sum and n are not equal
+            System.out.println(n + " is not a perfect number.");
+    }
 
-    public static void main(String args[])
-            {
-
-                int i,m=0,flag=0;
-                int n=3;//it is the number to be checked
-                m=n/2;
-                if(n==0||n==1){
-                    System.out.println(n+" is not prime number");
-                }else{
-                    for(i=2;i<=m;i++){
-                        if(n%i==0){
-                            System.out.println(n+" is not prime number");
-                            flag=1;
-                            break;
-                        }
-                    }
-                    if(flag==0)  { System.out.println(n+" is prime number"); }
-                }//end of else
-            }
-            }
-
+}
 
 
 
